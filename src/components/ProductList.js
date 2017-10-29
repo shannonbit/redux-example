a1594ad19d2a20d0f039cc5071aa17edda41d711
shannonbit <shannonbit@icloud.com>
2017-10-29 11:07:58 -0600

    Se agrega react-redux

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Button, Glyphicon } from 'react-bootstrap';
+import store from "../store";
+import {addToCart} from "../actionCreators";
 
 const styles = {
   products: {
@@ -47,7 +49,7 @@ class ProductList extends Component {
   }
 
   addToCart(product) {
-
+    store.dispatch(addToCart(product));
   }
 }
 
