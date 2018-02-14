f3bf0a1e9b2226b6ae1be87f05a634030f749f37
shannonbit <emmanuelle.laguna@televisatim.cim>
2018-02-14 14:06:02 -0600

    Curso Avanzado de React - 8. Introducción a Jest y Enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { configure } from "enzyme";
+import Adapter from 'enzyme-adapter-react-15';
+import { Provider } from "react-redux";
+import store from "./store";
+
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  configure(
+      <Provider store={store}>
+          <App />
+      </Provider>
+  );
 });
