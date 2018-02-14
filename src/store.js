74fe63917a3923c85a5e5e0d310f94c020e5cfc2
shannonbit <emmanuelle.laguna@televisatim.cim>
2018-02-14 12:33:26 -0600

    Curso Avanzado de React - 6. Operaciones Asincrónicas

--- src/store.js
+++ src/store.js
@@ -1,13 +1,18 @@
 import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 const reducer = (state, action) => {
-    if (action.type === "ADD_TO_CART") {
+    if (action.type === "REPLACE_PRODUCTS") {
+        return {
+            ...state,
+            products: action.products,
+        }
+    } else if (action.type === "ADD_TO_CART") {
         return {
             ...state,
             cart: state.cart.concat(action.product),
         }
-    }
-    if (action.type === "REMOVE_FROM_CART") {
+    } else if (action.type === "REMOVE_FROM_CART") {
         const CART = state.cart.filter(product => product.id !== action.product.id);
         return {
             ...state,
@@ -24,4 +29,4 @@ const logger = store => next => action => {
     return RESULT;
 }
 
-export default createStore(reducer, { cart: [] }, applyMiddleware(logger));
+export default createStore(reducer, { cart: [], products: [] }, applyMiddleware(logger, thunk));
